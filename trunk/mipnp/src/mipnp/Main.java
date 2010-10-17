f3a1c8aaa56c14c3a7afec320e18587bcf15a8c4
jvandenbussche <jvandenbussche@8aa536dd-9515-835a-30bf-d1dbb6cbee6e>
2010-10-17 13:46:46 +0000

    NetBeans project created.

---
+++ trunk/mipnp/src/mipnp/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * Main.java
+ * Created on Oct 17, 2010, 3:43:32 PM
+ */
+package mipnp;
+
+/**
+ *
+ * @author Jochem Van denbussche <jvandenbussche@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
