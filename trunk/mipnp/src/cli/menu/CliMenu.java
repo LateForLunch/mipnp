b9a3cabad1d3e84924af729ae351c09e16f5451b
jvandenbussche <jvandenbussche@8aa536dd-9515-835a-30bf-d1dbb6cbee6e>
2010-11-27 13:27:37 +0000

    CliMenu

---
+++ trunk/mipnp/src/cli/menu/CliMenu.java
@@ -0,0 +1,109 @@
+/*
+ * MiPnP, a minimal Plug and Play Server.
+ * Copyright (C) 2010  Jochem Van denbussche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * CliMenu.java
+ * Created on Nov 27, 2010, 1:41:06 PM
+ */
+package cli.menu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jochem Van denbussche <jvandenbussche@gmail.com>
+ */
+public class CliMenu {
+
+    private String question;
+    private String[] options;
+    private int defaultValue;
+    private Scanner scanner;
+
+    public CliMenu(String question, String[] options) {
+        this(question, options, -1);
+    }
+
+    public CliMenu(String question, String[] options, int defaultValue) {
+        this.question = question;
+        this.options = options;
+        setDefaultValue(defaultValue);
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int show() {
+        while (true) {
+            for (int i = 0; i < options.length; i++) {
+                System.out.format("[%d] %s\n", i+1, options[i]);
+            }
+            System.out.println();
+            System.out.print(question);
+            if (getDefaultValue() != -1) {
+                System.out.format(" [%d]", getDefaultValue());
+            }
+            System.out.print(": ");
+
+            String in = scanner.nextLine().trim();
+            if (getDefaultValue() != -1 && in.isEmpty()) {
+                return getDefaultValue();
+            }
+            int i = -1;
+            try {
+                i = Integer.parseInt(in);
+            } catch (NumberFormatException ex) {
+                System.out.println();
+                continue;
+            }
+            if (isValid(i)) {
+                return i;
+            }
+        }
+    }
+
+    public int getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(int defaultValue) {
+        if (isValid(defaultValue)) {
+            this.defaultValue = defaultValue;
+        } else {
+            this.defaultValue = -1;
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    private boolean isValid(int i) {
+        if (i > 0 && i <= options.length) {
+            return true;
+        }
+        return false;
+    }
+
+    // TEST
+    public static void main(String[] args) {
+        System.out.println(new CliMenu("test", new String[] {"t1", "t2"}, 2).show());
+    }
+}
