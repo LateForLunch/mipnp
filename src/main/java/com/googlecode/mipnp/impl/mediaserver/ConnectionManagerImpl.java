ded333c6c8730adaa2da0c82c3619133f2d3d1a3
jvandenbussche@gmail.com <jvandenbussche@gmail.com@8aa536dd-9515-835a-30bf-d1dbb6cbee6e>
2011-07-10 12:57:43 +0000

    moved the mediaserver package.

---
+++ src/main/java/com/googlecode/mipnp/impl/mediaserver/ConnectionManagerImpl.java
@@ -0,0 +1,43 @@
+/*
+ * MiPnP, a minimal Plug and Play Server.
+ * Copyright (C) 2010  Jochem Van denbussche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ConnectionManagerImpl.java
+ * Created on Jun 30, 2011, 3:36:22 PM
+ */
+package com.googlecode.mipnp.impl.mediaserver;
+
+import com.googlecode.mipnp.upnp.AbstractService;
+
+/**
+ *
+ * @author Jochem Van denbussche <jvandenbussche@gmail.com>
+ */
+class ConnectionManagerImpl extends AbstractService {
+
+    public ConnectionManagerImpl() {
+        super("upnp.org", "ConnectionManager", "ConnectionManager", "1");
+//        try {
+//            setDescriptionUri(new URI("/connectionmanager.xml"));
+//            setControlUri(new URI("/connectionmanager/control"));
+//            setEventUri(new URI("/connectionmanager/event"));
+//        } catch (URISyntaxException ex) {
+//            // This should not happen
+//        }
+    }
+}
