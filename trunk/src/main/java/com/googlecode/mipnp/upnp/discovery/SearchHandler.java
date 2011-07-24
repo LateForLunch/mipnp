b4042b7c9047b0f9f5155844695fdac47041b441
jvandenbussche@gmail.com <jvandenbussche@gmail.com@8aa536dd-9515-835a-30bf-d1dbb6cbee6e>
2011-07-24 12:19:05 +0000

    Added thread pool to SearchHandler. Implemented SearchResponder.

--- trunk/src/main/java/com/googlecode/mipnp/upnp/discovery/SearchHandler.java
+++ trunk/src/main/java/com/googlecode/mipnp/upnp/discovery/SearchHandler.java
@@ -22,18 +22,14 @@
  */
 package com.googlecode.mipnp.upnp.discovery;
 
-import com.googlecode.mipnp.http.MalformedHttpPacketException;
 import com.googlecode.mipnp.ssdp.SsdpConstants;
-import com.googlecode.mipnp.ssdp.SsdpRequest;
-import com.googlecode.mipnp.ssdp.SsdpResponse;
 import com.googlecode.mipnp.upnp.RootDevice;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -42,56 +38,35 @@ import java.util.List;
 class SearchHandler implements Runnable, SsdpConstants {
 
     private static final int BUF_SIZE = 512;
-    private static final int ADVERTISEMENT_DURATION = 1800;
 
     private RootDevice rootDevice;
     private MulticastSocket socket;
+    private ExecutorService threadPool;
 
     public SearchHandler(RootDevice rootDevice, MulticastSocket socket) {
         this.rootDevice = rootDevice;
         this.socket = socket;
+        this.threadPool = Executors.newCachedThreadPool();
     }
 
     public void run() {
-        while (!Thread.interrupted()) {
-            byte[] buf = new byte[BUF_SIZE];
-            DatagramPacket recv = new DatagramPacket(buf, buf.length);
-            try {
-                socket.receive(recv);
-                ByteArrayInputStream bais =
-                        new ByteArrayInputStream(
-                        recv.getData(), recv.getOffset(), recv.getLength());
-                SsdpRequest request = new SsdpRequest(bais);
-                if (request.isMsearch()) {
-                    handleRequest(recv, request);
+        try {
+            while (!Thread.interrupted()) {
+                byte[] buf = new byte[BUF_SIZE];
+                DatagramPacket recv = new DatagramPacket(buf, buf.length);
+                try {
+                    socket.receive(recv);
+                    threadPool.execute(
+                            new SearchResponder(rootDevice, socket, recv));
+                } catch (SocketException ex) {
+                    // Socket closed
+                    return;
+                } catch (IOException ex) {
+                    ex.printStackTrace(); // TODO
                 }
-            } catch (MalformedHttpPacketException ex) {
-                // Ignore packet
-                ex.printStackTrace(); // TODO: remove line if everything seems to work
-            } catch (SocketException ex) {
-                // Socket closed
-                return;
-            } catch (IOException ex) {
-                ex.printStackTrace(); // TODO
             }
-        }
-    }
-
-    private void handleRequest(DatagramPacket requestPacket, SsdpRequest request)
-            throws MalformedHttpPacketException, IOException {
-
-        InetAddress addr = requestPacket.getAddress();
-        int port = requestPacket.getPort();
-
-        List<SsdpResponse> responses =
-                SearchPacketFactory.createSearchResponseSet(
-                rootDevice, ADVERTISEMENT_DURATION, request);
-        for (SsdpResponse resp : responses) {
-            byte[] data = resp.getBytes();
-            DatagramPacket packet = new DatagramPacket(
-                    data, data.length,
-                    addr, port);
-            socket.send(packet);
+        } finally {
+            threadPool.shutdownNow();
         }
     }
 }
--- trunk/src/main/java/com/googlecode/mipnp/upnp/discovery/SearchResponder.java
+++ trunk/src/main/java/com/googlecode/mipnp/upnp/discovery/SearchResponder.java
@@ -22,16 +22,77 @@
  */
 package com.googlecode.mipnp.upnp.discovery;
 
+import com.googlecode.mipnp.http.MalformedHttpPacketException;
+import com.googlecode.mipnp.ssdp.SsdpRequest;
+import com.googlecode.mipnp.ssdp.SsdpResponse;
+import com.googlecode.mipnp.upnp.RootDevice;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.List;
+
 /**
  *
  * @author Jochem Van denbussche <jvandenbussche@gmail.com>
  */
 class SearchResponder implements Runnable {
 
-    public SearchResponder() {
+    private static final int ADVERTISEMENT_DURATION = 1800;
+
+    private RootDevice rootDevice;
+    private DatagramSocket socket;
+    private DatagramPacket searchPacket;
+
+    public SearchResponder(
+            RootDevice rootDevice,
+            DatagramSocket socket,
+            DatagramPacket searchPacket) {
+
+        this.rootDevice = rootDevice;
+        this.socket = socket;
+        this.searchPacket = searchPacket;
     }
 
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ByteArrayInputStream bais =
+                new ByteArrayInputStream(searchPacket.getData(),
+                searchPacket.getOffset(), searchPacket.getLength());
+        try {
+            SsdpRequest request = new SsdpRequest(bais);
+            if (request.isMsearch()) {
+                handleRequest(searchPacket, request);
+            }
+        } catch (MalformedHttpPacketException ex) {
+            // Ignore packet
+        } catch (IOException ex) {
+            ex.printStackTrace(); // TODO
+        }
+    }
+
+    private void handleRequest(
+            DatagramPacket requestPacket, SsdpRequest request)
+            throws MalformedHttpPacketException, IOException {
+
+        List<SsdpResponse> responses =
+                SearchPacketFactory.createSearchResponseSet(
+                rootDevice, ADVERTISEMENT_DURATION, request);
+        if (responses.size() <= 0) {
+            return;
+        }
+
+        InetAddress addr = requestPacket.getAddress();
+        int port = requestPacket.getPort();
+
+        // TODO: wait random time (MX header)
+
+        for (SsdpResponse resp : responses) {
+            byte[] data = resp.getBytes();
+            DatagramPacket packet = new DatagramPacket(
+                    data, data.length,
+                    addr, port);
+            socket.send(packet);
+        }
     }
 }
